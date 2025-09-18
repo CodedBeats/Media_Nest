18d1fac52e7401cbe62c180aba146507296ea0b3
CodedBeats <luca.haar@icloud.com>
2025-09-18 16:37:07 +1000

    feat: added simple manga, movie and series page

---
+++ src/pages/Movies.tsx
@@ -0,0 +1,23 @@
+// dependencies
+
+const Movies = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-full bg-gray-50">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">Movies Tracker</h1>
+            <p className="text-lg text-gray-600 mb-8">
+                movie stuff below
+            </p>
+            <button className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+                filter
+            </button>
+            <button className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+                Search
+            </button>
+            <p className="text-lg text-gray-600 mb-8">
+                movie entry
+            </p>
+        </div>
+    );
+};
+
+export default Movies;
