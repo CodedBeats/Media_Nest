99e22e50579b3f3a4a0bb24db9e46f172005d219
CodedBeats <luca.haar@icloud.com>
2025-09-20 18:50:07 +1000

    feat: simple utility to check empty/blank input

--- src/utility/manipulateStr.ts
+++ src/utility/manipulateStr.ts
@@ -11,3 +11,10 @@ export const lowercaseFirstLetter = (str: string) => {
     if (str.length === 0) return str;
     return str.charAt(0).toLowerCase() + str.slice(1);
 };
+
+// weird input validation i'm doing cause I'm weird
+export const checkEmptyInput = (input: string) => {
+    if (input.length === 0) return false;
+    if (/^\s*$/.test(input)) return false;
+    return true;
+};
