ea0878a4c5fb63277b269e6584a5c3f4b8fd995c
CodedBeats <luca.haar@icloud.com>
2025-09-26 20:32:04 +1000

    feat: implemented auth conditional rendering for navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+// dependencies
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// style
 import "./index.css";
+// app
 import App from "./App.tsx";
+// context
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <AuthProvider>
+            <App />
+        </AuthProvider>
     </StrictMode>
 );
