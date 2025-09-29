5073beacf8096ef56a6189be9a0e1b6581a215c4
CodedBeats <luca.haar@icloud.com>
2025-09-29 16:15:16 +1000

    refactor: MangaItem can have mangadex id for db and coverUrl for render

--- src/utility/interfaces.ts
+++ src/utility/interfaces.ts
@@ -4,13 +4,18 @@ import { type User } from "firebase/auth";
 
 export interface MangaItem {
     id?: string;
+    mangadexID?: string;
+    imgUrl?: string; // firebase optional
     title: string;
     author: string;
-    imgUrl: string;
     progress: string;
     rating: number;
     status: string;
 }
+// Manga item with guaranteed coverUrl
+export type MangaItemWithCover = MangaItem & { 
+    coverUrl: string 
+};
 
 export interface AuthContextType {
     user: User | null;
