166445ff8cb954c19d29b51737a7eaa42dc9b91e
CodedBeats <luca.haar@icloud.com>
2025-11-08 15:14:48 +1100

    style: mobile nav

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className="fixed flex items-center justify-between px-20 py-3 
+        <div className="fixed flex items-center justify-around sm:justify-between px-5 sm:px-5 md:px-20 py-3 
             bg-gray-800/50 w-full backdrop-blur-md z-50"
         >
             <NavItem to="/" label="Media Nest" />
