18098f91e56130a04e38941a2bba70663148a9dd
CodedBeats <luca.haar@icloud.com>
2025-11-08 17:00:34 +1100

    style: media status btn for mobile

--- src/components/btns/MediaStatusBtn.tsx
+++ src/components/btns/MediaStatusBtn.tsx
@@ -25,37 +25,26 @@ export const MediaStatusBtn = ({
     };
 
     return (
-        <div className="relative inline-block items-center justify-between text-left w-40">
+        <div className="relative w-full text-left sm:w-40">
             <button
-                className="flex items-center justify-between gap-2 px-3 py-1 text-white rounded
-                border-1 border-[#0CB321] border-solid hover:bg-[#0f661a] transition"
+                className="flex items-center justify-between w-full px-3 py-1 text-white rounded border border-[#0CB321] 
+                hover:bg-[#0f661a] transition text-sm"
                 onClick={toggleDropdown}
             >
                 <p>{currentStatus}</p>
                 {/* down arrow if admin */}
                 { !disabled && (
-                    <svg
-                    className="w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M19 9l-7 7-7-7"
-                        ></path>
+                    <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
                     </svg>
                 )}
             </button>
             {isOpen && (
-                <div className="absolute right-0 mt-0 w-40 bg-white border border-gray-300 rounded shadow-lg z-10">
+                <div className="absolute right-0 mt-1 w-full sm:w-40 bg-white border border-gray-300 rounded shadow-lg z-10">
                     {options.map((option) => (
                         <div
                             key={option}
-                            className="px-6 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition"
+                            className="px-4 py-2 bg-gray-200 text-gray-800 hover:bg-gray-300 transition text-sm"
                             onClick={() => handleOptionSelect(option)}
                         >
                             {option}
